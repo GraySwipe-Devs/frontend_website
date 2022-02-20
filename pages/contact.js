1b5e03fa8413628bbbd2a2f44031c44c6b5440e5
ShrinathNR <nr.shrinath@gmail.com>
2022-02-20 07:16:48 +0530

    done with contact page

--- pages/contact.js
+++ pages/contact.js
@@ -10,27 +10,33 @@ const contact = () => {
   return (
     <div className='contact-page'>
         <NavBar/>
-        <div className='contactpage-content flex justify-center '>
-            <div>
-                <h1 className=' font-bold text-4xl'>Contact us</h1>
+        <div className='contactpage-content flex justify-center space-x-9 mx-10'>
+            <div className='grow'>
+                <h1 className=' font-bold text-4xl m-10 text-center'>Contact us</h1>
                 <form className='flex flex-col'>
-                    <div className='mb-6'>
-                        <label>First Name</label>
-                        <input placeholder='First Name' />
+                    <div className='name flex'>
+                        <div className='mb-6 mr-6'>
+                            <label className='font-semibold'>First Name</label>
+                            <br />
+                            <input className='border-2 rounded-md mt-3 p-2' placeholder='First Name' />
+                        </div>
+                        <div className='mb-6'>
+                            <label className='font-semibold'>Last Name</label>
+                            <br />
+                            <input className='border-2 rounded-md mt-3 p-2' placeholder='Last Name' />
+                        </div>
                     </div>
-                    <div className='mb-6'>
-                        <label>Last Name</label>
-                        <input placeholder='Last Name' />
+                    
+                    <div className='mb-6 flex flex-col'>
+                        <label className='font-semibold'>Email Address</label>
+                        <input className='border-2 rounded-md mt-3 p-2' placeholder='Email Address' />
                     </div>
-                    <div className='mb-6'>
-                        <label>Email Address</label>
-                        <input placeholder='Email Address' />
-                    </div>
-                    <div className=''>
-                        <label>Your message</label>
+                    <div className='mb-6 flex flex-col'>
+                        <label className='-mb-3 font-semibold'>Your message</label>
                         <br />
-                        <textarea placeholder='Text-area' />
+                        <textarea className='border-2 rounded-md p-2 h-20 resize-none' placeholder='Text-area' />
                     </div>
+                    <button className='bg-dark-violet rounded-md text-white py-2 mb-12' type='submit'>SEND MESSAGE</button>
 
                     
                     {/* <label>
@@ -46,6 +52,9 @@ const contact = () => {
                     </label>
                     */}
                 </form>
+            </div>
+            <div className='grow-[2] max-w-3xl'>
+                <img src='https://i.imgur.com/zU1kVb7.png' />
             </div>
             
         </div>
